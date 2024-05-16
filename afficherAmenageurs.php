69fab58895c856c35781ad4cbbe854090c9b4a7f
LBombadil <152868387+LoBombadil@users.noreply.github.com>
2024-05-16 09:08:47 +0200

    Add files via upload

---
+++ afficherAmenageurs.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Reservation</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/reservation.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+        rel="stylesheet">
+</head>
+<ul class="row">
+        <li><a href="afficherAmenageurs.php" target="_blank">Afficher_Amenageurs</a></li>
+        <li><a href="afficherOperateurs.php" target="_blank">Afficher_Operateurs</a></li>
+        <li><a href="afficherBornes.php" target="_blank">Afficher_Bornes</a></li>
+        <li><a href="recherche.php" target="_blank">Recherche</a></li>
+    </ul>
+<body>
+
+</body>
+</html>
+
+
+
+
+<?php
