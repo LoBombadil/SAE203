cd06306208b67e88b6732ceaed34ca947deed985
LBombadil <152868387+LoBombadil@users.noreply.github.com>
2024-05-23 08:59:16 +0200

    Update supprimerAmenageurs.php

--- supprimerAmenageurs.php
+++ supprimerAmenageurs.php
@@ -5,6 +5,11 @@
     <title>Ajout</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="BaseBootstrap/css/ajouterAmenageur.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,29 +19,50 @@
           rel="stylesheet">
 </head>
 <body>
-<ul class="row">
-    <li><a href="afficherAmenageurs.php" target="_blank">Afficher_Amenageurs</a></li>
-    <li><a href="ajouterAmenageur.php" target="_blank">Ajouter_Amenageurs</a></li>
-    <li><a href="supprimerAmenageurs.php" target="_blank">Supprimer_Amenageurs</a></li>
-    <li><a href="recherches.php" target="_blank">Recherches</a></li>
-</ul>
 
-<h1>Amenageurs</h1>
-<section class="col-md-7">
-    <form action="supprimerAmenageurs.php" method="get">
-        <fieldset>
-            <legend>Supprimer Amenageur</legend>
-            <label for="siren"> N° siren : </label>
-            <input type="text" name="siren" id="siren"> <br>
-            <label for="nom"> Nom : </label>
-            <input type="text" name="nom" id="nom"><br>
-            <label for="contact"> Contact : </label>
-            <input type="text" name="contact" id="contact">
-        </fieldset>
-        <input type="submit" value="Ajouter">
-    </form>
-</section>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="afficherAmenageurs.php" target="_blank">Afficher</a></li>
+                <li><a href="ajouterAmenageur.php" target="_blank">Ajouter</a></li>
+                <li><a href="supprimerAmenageurs.php" target="_blank">Supprimer</a></li>
+                <li><a href="recherches.php" target="_blank">Recherches</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
+<div class="container-fluid text-center">
+    <div class="row content">
+        <div class="col-sm-2 sidenav">
+
+        </div>
+        <div class="col-sm-8 text-left">
+            <h1>Amenageur</h1>
+            <section class="col-md-7">
+                <form action="supprimerAmenageurs.php" method="get">
+                    <fieldset>
+                        <legend>Supprimer Amenageur</legend>
+                        <label for="siren"> N° siren : </label>
+                        <input type="text" name="siren" id="siren"> <br>
+                        <label for="nom"> Nom : </label>
+                        <input type="text" name="nom" id="nom"><br>
+                        <label for="contact"> Contact : </label>
+                        <input type="text" name="contact" id="contact">
+                    </fieldset>
+                    <input type="submit" value="Supprimer">
+                </form>
+            </section>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
 <?php
 // récupération des données du formulaire
 $siren=$_GET["siren"];
