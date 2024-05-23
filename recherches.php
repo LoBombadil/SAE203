b5faa271ee6dfded710bc18540077b5c0699aa63
LBombadil <152868387+LoBombadil@users.noreply.github.com>
2024-05-23 09:08:47 +0200

    Update recherches.php

--- recherches.php
+++ recherches.php
@@ -1,3 +1,81 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
 
-?>
+<head>
+    <title>Ajout</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="BaseBootstrap/css/ajouterAmenageur.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+          rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="afficherAmenageurs.php" target="_blank">Afficher</a></li>
+                <li><a href="ajouterAmenageur.php" target="_blank">Ajouter</a></li>
+                <li><a href="supprimerAmenageurs.php" target="_blank">Supprimer</a></li>
+                <li><a href="recherches.php" target="_blank">Recherches</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<h1>Amenageurs</h1>
+<section class="col-md-7">
+
+    <p>Entreprises ayant mis en place les stations de recharge.</p>
+    <label for "recherche"> La commune est</label>
+    <select id="recherche name="recherche">
+        <option value='0'></option>
+
+        <?php
+
+        try {
+            // Récupération des données
+            $servername = "127.0.0.1";
+            $dbname = "BUTRT1_lg409538";
+            $username = "lg409538";
+            $userpassword = "MDP_lg409538";
+            echo 'ca ma';
+            $lienBDD = new PDO("mysql:host=$servername;dbname=$dbname", "$username", "$userpassword");
+            $lienBDD->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Acces BDD réaliser";
+
+            //Prepare la requête
+            $requeteSQL = $lienBDD->prepare("SELECT nom, commun
+                                                   FROM operateurs INNER JOIN station
+                                                   ON operateurs.id = station.operateur_id");
+            $requeteSQL->execute();
+
+
+            // Affichage des résultats dans un tableau
+            $tab = $requeteSQL->fetchAll(PDO::FETCH_ASSOC);
+                while ($donnees = $tab) {
+                    echo "<option value='".$donnees['commun']."'>".$donnees['commun']."</option>";
+                }
+        }
+        catch (PDOException $e)
+        {
+            die("Erreur : ").$e-> getMessage();
+        }
+        ?>
+
+    </table>
+</section>
+</body>
+</html>
