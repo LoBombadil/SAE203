29726bce70a22ed20f30f3d3b1e4df8aaa801e31
LBombadil <152868387+LoBombadil@users.noreply.github.com>
2024-05-23 12:58:48 +0200

    Update supprimerAmenageurs.php

--- supprimerAmenageurs.php
+++ supprimerAmenageurs.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 
 <head>
-    <title>Ajout</title>
+    <title>Supprimer</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
