ddb96e0a6f61e26c8ff0acf8991483e09a9f5275
LBombadil <152868387+LoBombadil@users.noreply.github.com>
2024-05-23 09:54:40 +0200

    Update recherches.php

--- recherches.php
+++ recherches.php
@@ -2,77 +2,83 @@
 <html lang="fr">
 
 <head>
-    <title>Rechercher</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="BaseBootstrap/css/ajouterAmenageur.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
-          rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
-          rel="stylesheet">
+  <title>Rechercher</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" href="BaseBootstrap/css/ajouterAmenageur.css">
+
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 </head>
+
 <body>
 
-<nav class="navbar navbar-inverse">
+  <nav class="navbar navbar-inverse">
     <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">Logo</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-                <li><a href="afficherAmenageurs.php" target="_blank">Afficher</a></li>
-                <li><a href="ajouterAmenageur.php" target="_blank">Ajouter</a></li>
-                <li><a href="supprimerAmenageurs.php" target="_blank">Supprimer</a></li>
-                <li><a href="recherches.php" target="_blank">Recherches</a></li>
-            </ul>
-        </div>
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Logo</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="afficherAmenageurs.php">Afficher</a></li>
+          <li><a href="ajouterAmenageur.php">Ajouter</a></li>
+          <li><a href="supprimerAmenageurs.php">Supprimer</a></li>
+          <li class="active"><a href="#">Recherches</a></li>
+        </ul>
+      </div>
     </div>
-</nav>
-<h1>Amenageurs</h1>
-<section class="col-md-7">
+  </nav>
 
-    <label for "recherche"> Quel est le nom d'operateurs dont vous voulez ?</label>
+  <h1>Rechercher des aménageurs</h1>
+
+  <section class="col-md-7">
+
+    <label for="recherche">Quel est le nom de l'opérateur que vous recherchez ?</label>
     <select id="recherche" name="recherche">
-        <option value='0'></option>
+      <option value="">Sélectionner un opérateur</option>
 
-        <?php
+      <?php
 
-        try {
-            // Récupération des données
-            $servername = "127.0.0.1";
-            $dbname = "BUTRT1_lg409538";
-            $username = "lg409538";
-            $userpassword = "MDP_lg409538";
-            echo 'ca marche';
-            $lienBDD = new PDO("mysql:host=$servername;dbname=$dbname", "$username", "$userpassword");
-            $lienBDD->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Acces BDD réaliser";
+      try {
+        // Database connection details (replace with your actual credentials)
+        $servername = "127.0.0.1";
+        $dbname = "BUTRT1_lg409538";
+        $username = "lg409538";
+        $userpassword = "MDP_lg409538";
 
-            //Prepare la requête
-            $requeteSQL = $lienBDD->prepare("SELECT o.nom, s.commune FROM operateurs o INNER JOIN stations s ON o.id = s.operateur_id;");
-            $requeteSQL->execute();
+        $lienBDD = new PDO("mysql:host=$servername;dbname=$dbname", $username, $userpassword);
+        $lienBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        // Prepare the SQL query
+        $requeteSQL = $lienBDD->prepare("SELECT o.nom, s.commune FROM operateurs o INNER JOIN stations s ON o.id = s.operateur_id;");
+        $requeteSQL->execute();
 
-            // Affichage des résultats dans un tableau
-            $tab = $requeteSQL->fetchAll(PDO::FETCH_ASSOC);
-                while ($donnees = $tab) {
-                    echo "<option value='".$donnees['o.nom']."'>".$donnees['s.commune']."</option>";
-                }
+        // Fetch results as associative array
+        $tab = $requeteSQL->fetchAll(PDO::FETCH_ASSOC);
+
+        if (count($tab) > 0) {
+          foreach ($tab as $donnees) {
+            echo "<option value='" . $donnees['nom'] . "'>" . $donnees['nom'] . " (" . $donnees['commune'] . ")</option>";
+          }
+        } else {
+          echo "<option value=''>Aucun opérateur trouvé</option>";
         }
-        catch (PDOException $e)
-        {
-            die("Erreur : ").$e-> getMessage();
-        }
-        ?>
 
-    </table>
-</section>
+      } catch (PDOException $e) {
+        die("Erreur : " . $e->getMessage());
+      }
+
+      ?>
+
+    </select>
+
+  </section>
+
 </body>
 </html>
