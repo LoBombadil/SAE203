01776f1fbe8c2f013b69f50f3c17fbe54270ed5d
LBombadil <152868387+LoBombadil@users.noreply.github.com>
2024-05-23 11:37:09 +0200

    Update afficherAmenageurs.php

--- afficherAmenageurs.php
+++ afficherAmenageurs.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 
 <head>
-    <title>Affichage</title>
+    <title>Ajout</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
