f137a16ab7f911aac437eb8cb930f4d1b96b6ca3
ytsangue <152924097+ytsangue@users.noreply.github.com>
2024-05-28 09:30:09 +0200

    Update ajouterAmenageur.php

--- ajouterAmenageur.php
+++ ajouterAmenageur.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 
 <head>
-    <title>Ajout</title>
+    <title>Ajouter</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
@@ -13,10 +13,6 @@
     <link rel="stylesheet" href="BaseBootstrap/css/ajouterAmenageur.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
-          rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
-          rel="stylesheet">
 </head>
 <body>
 
@@ -28,8 +24,8 @@
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
                 <li><a href="afficherAmenageurs.php">Afficher</a></li>
-                <li><a href="ajouterAmenageur.php">Ajouter</a></li>
-                <li><a href="supprimerAmenageurs.php">Supprimer</a></li>
+                <li><a href="ajouterAmenageur.html">Ajouter</a></li>
+                <li><a href="supprimerAmenageurs.html">Supprimer</a></li>
                 <li><a href="recherches.php">Recherches</a></li>
             </ul>
         </div>
@@ -44,18 +40,42 @@
         <div class="col-sm-8 text-left">
             <h1>Amenageur</h1>
             <section class="col-md-7">
-                <form action="ajouterAmenageur.php" method="get">
-                    <fieldset>
-                        <legend>Ajouter Amenageur</legend>
-                        <label for="siren"> N° siren : </label>
-                        <input type="text" name="siren" id="siren"> <br>
-                        <label for="nom"> Nom : </label>
-                        <input type="text" name="nom" id="nom"><br>
-                        <label for="contact"> Contact : </label>
-                        <input type="text" name="contact" id="contact">
-                    </fieldset>
-                    <input type="submit" value="Ajouter">
-                </form>
+            <?php
+                // récupération des données du formulaire
+                $siren=$_GET["siren"];
+                $nom=$_GET["nom"];
+                $contact=$_GET["contact"];
+            
+                try {
+                    // Récupération des données
+                    $servername = "127.0.0.1";
+                    $dbname = "BUTRT1_lg409538";
+                    $username = "lg409538";
+                    $userpassword = "MDP_lg409538";
+
+                    $lienBDD = new PDO("mysql:host=$servername;dbname=$dbname", "$username", "$userpassword");
+                    $lienBDD->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    // Preparation de la requête
+                    $requeteSQL = $lienBDD->prepare("INSERT INTO amenageurs (siren, nom, contact) VALUES (:siren, :nom, :contact)");
+
+                    // Liaison des données avec la requête SQL
+                    $requeteSQL->bindParam(":siren",$siren);
+                    $requeteSQL->bindParam(":nom",$nom);
+                    $requeteSQL->bindParam(":contact",$contact);
+
+                    // Exécution de la requête SQL
+                    $requeteSQL->execute();
+                    echo "<p class='panel panel-success'>Votre aménageur a bien été ajouté à la base de donnée !</p>";
+                }
+                catch (PDOException $e)
+                {
+                    // traitement d'erreurs
+                    die ("Erreur : ".$e->getMessage());
+                }
+	?>
+		<a class="btn btn-primary" href="ajouterAmenageur.html" role="button">Ajouter un autre amenageur</a>
+		<a class="btn btn-outline-dark" href="supprimerAmenageurs.html" role="button">Supprimer un amenageur</a>
             </section>
         </div>
     </div>
@@ -69,40 +89,3 @@
     
 </body>
 </html>
-
-
-<?php
-    // récupération des données du formulaire
-    $siren=$_GET["siren"];
-    $nom=$_GET["nom"];
-    $contact=$_GET["contact"];
-    
-        try {
-            // Récupération des données
-            $servername = "127.0.0.1";
-            $dbname = "BUTRT1_lg409538";
-            $username = "lg409538";
-            $userpassword = "MDP_lg409538";
-
-            $lienBDD = new PDO("mysql:host=$servername;dbname=$dbname", "$username", "$userpassword");
-            $lienBDD->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            // Preparation de la requête
-            $requeteSQL = $lienBDD->prepare("INSERT INTO amenageurs (siren, nom, contact) VALUES (:siren, :nom, :contact)");
-
-            // Liaison des données avec la requête SQL
-            $requeteSQL->bindParam(":siren",$siren);
-            $requeteSQL->bindParam(":nom",$nom);
-            $requeteSQL->bindParam(":contact",$contact);
-
-            // Exécution de la requête SQL
-            $requeteSQL->execute();
-            echo "<p>Votre aménageur a bien été ajouté à la base de donnée !</p>";
-        }
-        catch (PDOException $e)
-        {
-            // traitement d'erreurs
-            die ("Erreur : ".$e->getMessage());
-        }
-?>
-
